3c312b201c484f62a6d4956a6dc7fe0c59fc0638
Keith O'Malley <keithcharlieom@gmail.com>
2015-11-26 11:23:41 +0000

    Added collisions and smooth physics

--- node/public/js/player.js
+++ node/public/js/player.js
@@ -1,29 +1,58 @@
 function Player (x,y,w,h,c){
   this.x = x;
   this.y = y;
-  this.w = w;
-  this.h = h;
+  this.width = w;
+  this.height = h;
+  this.temp_height = h;
   this.c = c;
+  this.velY = 0;
+  this.velX = 0;
+  this.friction = 0.8;
+  this.speed = 3;
+  this.gravity = 0.2;
+  this.jumping = false;
+  this.grounded = false;
 }
 
-Player.prototype.draw = function(context,slide,jump){
+Player.prototype.update = function(context,slide,jump){
+  if(jump == 1){
+    if(!this.jumping && this.grounded){
+      this.jumping = true;
+      this.grounded = false;
+      this.velY = -this.speed * 2;
+    }
+  }
+
+  if(slide == 1){
+    this.height = this.temp_height / 2;
+  } else {
+    this.height = this.temp_height;
+  }
+
+
+  if (this.velX < this.speed) {
+    this.velX++;
+  }
+
+  if (this.x >= ((context.canvas.width / 2) -  this.width/2 )){
+    this.velX = 0;
+  }
+
+
+  this.velX *= this.friction;
+  this.velY += this.gravity;
+
+
+
+} // end update
+
+
+Player.prototype.draw = function(context){
 
   context.shadowBlur=10;
   context.shadowColor="rgba(0,0,0,0.2)";
   context.fillStyle=this.c;
-
-  if(this.y < (context.canvas.height - this.h )){
-    this.y += 5;
-  }
-
-  if(jump == 1){
-    this.y -= 20;
-    context.fillRect(this.x,this.y,this.w,this.h);
-  } else if(slide == 1){
-    context.fillRect(this.x,this.y + this.w,this.h,this.w);
-  } else {
-    context.fillRect(this.x,this.y,this.w,this.h);
-  }
+  context.fillRect(this.x,this.y,this.width,this.height);
 
   context.shadowBlur=0;
 }
---
+++ node/public/js/raf.js
@@ -0,0 +1,5 @@
+// Request Animation frame shim
+(function() {
+  var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
